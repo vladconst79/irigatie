261e3bdef57dad30706ad6f9e982fe7af383b747
Vlad Constantinescu <vlad.constantinescu@totaltech.ro>
2018-10-07 10:10:40 +0300

    primele adaugari

---
+++ web/header.php
@@ -0,0 +1,29 @@
+<?php
+session_set_cookie_params(28800,"/");
+session_start();
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+?>
+
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>Programator irigiatie</title>
+    <link rel="stylesheet" href="css/bootstrap.css" />
+    <link rel="stylesheet" href="css/styles.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div id="head">
+    <div class="row">
+        <div class="col">
+            <img src="images/header-irigatii.jpg">
+        </div>
+    </div>
+</div>
+</body>
+

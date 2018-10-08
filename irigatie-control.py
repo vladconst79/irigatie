5e082cf24198c9c528e1134d27a2cf584732ec24
Vlad Constantinescu <vlad.constantinescu@totaltech.ro>
2018-10-08 23:11:09 +0300

    GPIO.cleanup

--- irigatie-control.py
+++ irigatie-control.py
@@ -276,5 +276,4 @@ except KeyboardInterrupt:
         print('\033[41m' + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")) +
               'Bucla intrerupta cu <CTRL>+<C>\033[0m')
     syslog.syslog(syslog.LOG_ERR, 'Bucla intrerupta cu <CTRL>+<C>')
-    GPIO.setup([R_TRAF, R_IRI1, R_IRI2, R_IRI3, R_IRI4, L_RED, L_GREEN, L_BLUE], GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup([S_RAIN, B_BUT1, B_BUT2, B_BUT3, B_BUT4], GPIO.IN)
+    GPIO.cleanup()

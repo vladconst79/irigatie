d0b6864d6d413102d271ffd911c4b76d2afa70a8
Vlad Constantinescu <vlad.constantinescu@totaltech.ro>
2018-11-10 11:14:11 +0200

    gpio-stat.sh actualizare pini

--- gpio-stat.sh
+++ gpio-stat.sh
@@ -7,7 +7,7 @@ echo "\033[0mReleu 2:" `gpio -g read 20` ;
 echo "\033[0mReleu 3:" `gpio -g read 16` ;
 echo "\033[0mReleu 4:" `gpio -g read 12` ;
 echo "\033[0;94mSenzor ploaie:" `gpio -g read 23` ;
-echo "\033[0;31mLED rosu:" `gpio -g read 28` ;
+echo "\033[0;31mLED rosu:" `gpio -g read 18` ;
 echo "\033[0;32mLED verde:" `gpio -g read 19` ;
 echo "\033[0;36mLED albastru: " `gpio -g read 13` ;
 echo "\033[0;44mButon 1:" `gpio -g read 9` ;
